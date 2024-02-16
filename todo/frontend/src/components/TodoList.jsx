fb04ed8912129390f22d074352321afb9ec453c9
Ravin Yadav <ravinyadav973@gmail.com>
2024-02-16 14:18:07 +0530

    completed the search bar toggle

--- todo/frontend/src/components/TodoList.jsx
+++ todo/frontend/src/components/TodoList.jsx
@@ -6,6 +6,8 @@ import "react-toastify/dist/ReactToastify.css";
 import Pagination from "./Pagination";
 import TodoFilter from "./TodoFilter";
 import { fetchTodo } from "../utils/todoApi";
+import { FaSearch } from "react-icons/fa";
+import { RxCross2 } from "react-icons/rx";
 
 function TodoList() {
   const { todo, setTodo } = useContext(todoContext);
@@ -18,11 +20,11 @@ function TodoList() {
   const todosPerPage = 8;
   const [active, setActive] = useState("");
   const [search, setSearch] = useState("");
+  const [searchIcon, setSearchIcon] = useState(true);
 
   useEffect(() => {
     fetchTodo(todo, setTodo);
   }, [setTodo]);
-
 
   const handleSearchChange = (e) => {
     setSearch(e.target.value);
@@ -31,7 +33,7 @@ function TodoList() {
   const handleFilterChange = (event) => {
     setSelectedFilter(event.target.value);
     if (selectedFilter !== "textFilter") {
-      setSearch("")
+      setSearch("");
     }
   };
 
@@ -88,11 +90,6 @@ function TodoList() {
     }
   }, [todo, selectedFilter, currentPage, todosPerPage, search]);
 
-  // const handleFilterChange = (event) => {
-  //   setSelectedFilter(event.target.value);
-  //   setCurrentPage(1); // Reset current page when changing the filter
-  // };
-
   const handlePageChange = (page) => {
     setCurrentPage(page);
     localStorage.setItem("currentPage", page.toString());
@@ -103,7 +100,6 @@ function TodoList() {
   const endIndex = startIndex + todosPerPage;
   const visibleTodos = filteredTodos.slice(startIndex, endIndex);
 
-
   return (
     <>
       <div className=" bg-red-700 w-auto h-[42.5rem] relative  dark:bg-gray-800 dark:border-gray-700 shadow-md sm:rounded-lg">
@@ -111,49 +107,47 @@ function TodoList() {
           {/* Filters and Search Bar */}
           {/* Include the TodoFilter component */}
           <div className="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
-
-          <TodoFilter
-            selectedFilter={selectedFilter}
-            handleFilterChange={handleFilterChange}
-            search={search}
-          />
-
-          {/* Search bar */}
-          <label htmlFor="table-search" className="sr-only">
-            Search
-          </label>
-          <div className="relative">
-            <div className="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
-              {/* Search Icon */}
-              <svg
-                className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"></path>
-              </svg>
-            </div>
-            {/* Search Input */}
-            <input
-              type="text"
-              id="table-search"
-              className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search htmlFor items"
-              value={search}
-              onChange={handleSearchChange}
-              onClick={(e) => {
-                setSelectedFilter("textFilter")
-              } }
+            <TodoFilter
+              selectedFilter={selectedFilter}
+              handleFilterChange={handleFilterChange}
+              search={search}
             />
-            <button 
-            className=" text-white"
-            onClick={(e) => {
-              setSelectedFilter("newest")
-              setSearch("")
-            }}
-            >
-              button
-            </button>
-          </div>
+
+            {/* Search bar */}
+            <label htmlFor="table-search" className="sr-only">
+              Search
+            </label>
+            <div className=" flex">
+              {/* <div className="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
+             
+            </div> */}
+              {/* Search Input */}
+              <input
+                type="text"
+                id="table-search"
+                className="block p-2 ps-10 outline-none text-sm text-gray-900 border border-gray-300 rounded-lg w-80 rounded-e-none bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search htmlFor items..."
+                value={search}
+                onChange={handleSearchChange}
+                onClick={(e) => {
+                  setSelectedFilter("textFilter");
+                  setSearchIcon(!searchIcon);
+                }}
+              />
+              <button
+                className={`{ rounded-s-none rounded-lg p-3 ${
+                  searchIcon ? "hidden" : ""
+                } text-black bg-slate-300 }`}
+                disabled={searchIcon ? true : false}
+                onClick={(e) => {
+                  setSelectedFilter("newest");
+                  setSearch("");
+                  setSearchIcon(!searchIcon);
+                }}
+              >
+                {searchIcon ? <FaSearch /> : <RxCross2 />}
+              </button>
+            </div>
           </div>
 
           {/* Todo List Table */}
